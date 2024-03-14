c24bca8e1a44fbdc023dea1a3ec5e7267d45fe6e
GabrielVeloso077 <163217664+GabrielVeloso077@users.noreply.github.com>
2024-03-14 12:18:29 -0300

    Update matematica.c

--- matematica.c
+++ matematica.c
@@ -1,14 +1,21 @@
 #include "matematica.h"
 #include <math.h>
 
+
+// Implementação da função calcular_seno, que calcula o seno de um ângulo em radianos.
 float calcular_seno(float angulo_radianos) {
+    // Usa a função sin da biblioteca math.h para calcular o seno do ângulo.
     return sin(angulo_radianos);
 }
 
+// Implementação da função calcular_cosseno, que calcula o cosseno de um ângulo em radianos.
 float calcular_cosseno(float angulo_radianos) {
+    // Usa a função cos da biblioteca math.h para calcular o cosseno do ângulo.
     return cos(angulo_radianos);
 }
 
+// Implementação da função calcular_tangente, que calcula a tangente de um ângulo em radianos.
 float calcular_tangente(float angulo_radianos) {
+    // Usa a função tan da biblioteca math.h para calcular a tangente do ângulo.
     return tan(angulo_radianos);
 }
